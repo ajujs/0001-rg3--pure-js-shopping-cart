5373ecebff09d11b29676343dae4aeb828dcc022
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2021-01-01 19:03:05 -0300

    Project HTML and CSS

---
+++ index.js
@@ -0,0 +1,4 @@
+const emptyCardElement = document.querySelector('#cardProductListEmpty');
+const cardListElement = document.querySelector('#cardProductList');
+
+emptyCardElement.style.display = 'none';
